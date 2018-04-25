cb4c3b038da6cb55b5c6134cfc6243f7b077b46e
Nix2k <mihail.bokov@gmail.com>
2018-04-25 21:53:42 +0300

    3

--- index.php
+++ index.php
@@ -8,17 +8,22 @@
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    //$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+    $keyboard = [["Погода"]]; //Клавиатура
 
     if($text){
-         if ($text == "/start") {
-            $reply = "Добро пожаловать в бота! Задайте ваш вопрос.";
-            //$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-        }else{
-        	$reply = 'Ваш вопрос "<i>'.$text.'</i>" передан на обработку.';
-        	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+        switch ($text) {
+            case '/start':
+                $reply = "Добро пожаловать в бота!";
+                break;
+            case 'Погода':
+                $reply = "Прогноз погоды";
+                break;
+            default:
+                $reply = "Неизвестная команда";
+                break;
         }
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     }else{
     	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
